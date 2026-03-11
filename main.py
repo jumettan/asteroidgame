b0ced168d5d21b9c424b9dbd062b71d06116394c
metanju <juho.mettanen@elisanet.fi>
2026-03-11 12:51:02 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+    print("Starting Asteroids with pygame version: VERSION","Screen width:",SCREEN_WIDTH,"Screen height:", SCREEN_HEIGHT)
+
+
+if __name__ == "__main__":
+    main()
