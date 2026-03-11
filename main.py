bda92b441b3ea3e48a06ea8fd13a1c5da952a28c
metanju <juho.mettanen@elisanet.fi>
2026-03-11 13:09:23 +0200

    pygame windows

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 def main():
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    while True:
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        else:
+            pass
     print("Starting Asteroids with pygame version: VERSION","Screen width:",SCREEN_WIDTH,"Screen height:", SCREEN_HEIGHT)
-
 
 if __name__ == "__main__":
     main()
