9664185311d3c93b84c5d5e4be49d07a71639239
metanju <juho.mettanen@elisanet.fi>
2026-03-11 15:12:48 +0200

    collision detection, movement via wasd keys and asteroid movement

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import *
+import sys
 
 def main():
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    #player
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+    #asteroid
+    Asteroid.containers = (asteroids,updatable,drawable)
+
+    #field
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
+
     while True:
         log_state()
         screen.fill("black")
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         else:
             pass
+        dt = clock.tick(60)/1000
     print("Starting Asteroids with pygame version: VERSION","Screen width:",SCREEN_WIDTH,"Screen height:", SCREEN_HEIGHT)
 
 if __name__ == "__main__":
